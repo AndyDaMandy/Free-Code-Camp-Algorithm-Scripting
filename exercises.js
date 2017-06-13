fa8a682b57b9f438a587e03deac321ee70b4a93f
AndyDaMandy <maldonado.andy.m@gmail.com>
2017-06-13 13:54:57 -0400

    Create exercises.js

--- exercises.js
+++ exercises.js
@@ -31,3 +31,31 @@ var array = [];
 factorialize(5);
 // Here's challenge two. I was being tripped up hard because my array was taking in the value of 0. But using shift allowed me to remove the 0.
 // Removing the zero was all I needed to do to have .reduce properly multiply the array. The one multiplies it by one so that it returns the answer.
+
+
+// Challenge 3
+
+function palindrome(str) {
+  var removed = str.replace(/[^a-zA-Z0-9]/g, '');
+ var array = [];
+ array = str.split("");
+  array = str.toLowerCase();
+  
+ var backwards = [];
+  backwards = str.split("");
+  backwards = str.toLowerCase();
+ 
+  var test = backwards.reverse();
+  console.log(test);
+  
+  if (array === backwards){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+palindrome("Testing");
+
+// Still stuck on this. I'm making some progress though... Hopefully I can figure out how to get the method (.reverse) to work on my backwards variable...
