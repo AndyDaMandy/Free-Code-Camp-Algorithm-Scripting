b4cf09ca3a699946c57448ac8211722567a8c9b9
AndyDaMandy <maldonado.andy.m@gmail.com>
2017-06-15 13:46:47 -0400

    Create exercises.js

--- exercises.js
+++ exercises.js
@@ -61,3 +61,28 @@ function palindrome(str) {
 palindrome("rac___Aecar");
 //Output = false
 
+// Challenge 4
+function findLongestWord(str) {
+
+  var array = [];
+  var wordLength = 0;
+  
+  array = str.split(" ");
+  
+  for (var i = 0; i < array.length; i++){
+    if (wordLength < array[i].length){
+      wordLength = array[i].length;
+    }
+    
+  }
+  return wordLength;
+}
+
+findLongestWord("The quick brown fox jumped BN over the lazy dog");
+/* I had a bit of trouble with this one (as I always do). I knew I needed to convert into a array.
+I also knew that I needed to loop, but my biggest problem was figuring out HOW to get it to stop when it finds the value.
+The truth is after some googling I found out that I should use an if statement to handle this. My if statement replaces the value of
+of the variable "wordLength" which isa counter for the highest value. What will happen is that if wordLength is LESS than a value
+the loop comes across, it will take the value of of the loop. It will do this until the loop runts out ( i < array.length will run its course)
+which will then lead the final value of wordLength to be the highest value. It's actually pretty brilliant. 
+*/
